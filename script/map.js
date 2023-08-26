b812790b6c69182284e20cdaa2e7ea2bec10ad05
Ramji <ramjirenanda02@gmail.com>
2023-08-26 18:49:52 +0700

    menambahkan button, modal, dan list cafe rekomendasi; mengganti logo univ

--- script/map.js
+++ script/map.js
@@ -10,7 +10,43 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
 // Menambahkan layer untuk data cafe_v2.geojson
 var cafeLayer = L.geoJSON();
 
-// Memuat data cafe_v2.geojson menggunakan AJAX
+// Geoloc
+var locateControl = L.control
+  .locate({
+    position: 'bottomright',
+    drawCircle: true,
+    follow: true,
+    setView: true,
+    keepCurrentZoomLevel: false,
+    markerStyle: {
+      weight: 1,
+      opacity: 0.8,
+      fillOpacity: 0.8,
+    },
+    circleStyle: {
+      weight: 1,
+      clickable: false,
+    },
+    icon: 'fas fa-crosshairs',
+    metric: true,
+    strings: {
+      title: 'Click for Your Location',
+      popup: "You're here. Accuracy {distance} {unit}",
+      outsideMapBoundsMsg: 'Not available',
+    },
+    locateOptions: {
+      maxZoom: 16,
+      watch: true,
+      enableHighAccuracy: true,
+      maximumAge: 10000,
+      timeout: 10000,
+    },
+  })
+  .addTo(map);
+
+// Pagination
+var itemsPerPage = 10;
+
 // Memuat data cafe_v2.geojson menggunakan AJAX
 fetch('data/cafe_v2.geojson')
   .then((response) => response.json())
@@ -156,42 +192,17 @@ fetch('data/cafe_v2.geojson')
     console.error('Error loading cafe_v2.geojson:', error);
   });
 
-
 fetch('data/uni_buffer_2km_gcs.geojson')
   .then((response) => response.json())
   .then((data) => {
-    // Fungsi untuk mengubah warna saat kursor bergerak melewati fitur
-    function highlightFeature(e) {
-      var layer = e.target;
-      layer.setStyle({
-        fillOpacity: 0.3, // Warna berubah saat kursor bergerak melewati
-      });
-    }
-
-    // Fungsi untuk mengembalikan warna semula saat kursor keluar dari fitur
-    function resetHighlight(e) {
-      var layer = e.target;
-      layer.setStyle({
-        fillOpacity: 0, // Mengembalikan ke hollow saat kursor keluar
-      });
-    }
-
-    // Fungsi untuk mengatur interaksi hover pada setiap fitur (layer) individu
-    function onEachFeature(feature, layer) {
-      layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-      });
-    }
-
     // Buat layer dari data polygon dengan simbologi yang diinginkan dan atur interaksi hover
     var bufferLayer = L.geoJSON(data, {
       style: function (feature) {
         return {
           fillColor: 'blue', // Warna biru muda
-          fillOpacity: 0.1, // Set fillOpacity 0 untuk membuat berlubang (hollow)
+          fillOpacity: 0, // Set fillOpacity 0 untuk membuat berlubang (hollow)
           color: 'blue', // Warna garis
-          weight: 2, // Ketebalan garis
+          weight: 0, // Ketebalan garis
         };
       },
       onEachFeature: onEachFeature, // Atur interaksi hover pada setiap fitur
@@ -217,14 +228,11 @@ fetch('data/uni_point.geojson')
   .then((data) => {
     var pointLayer = L.geoJSON(data, {
       pointToLayer: function (feature, latlng) {
-        // Membuat marker pada setiap titik dengan ikon bawaan Leaflet berwarna merah
         return L.marker(latlng, {
-          icon: L.icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41],
+          icon: L.AwesomeMarkers.icon({
+            icon: 'school', // Icon name from Font Awesome (e.g., "coffee" for coffee cup)
+            markerColor: 'red',
+            prefix: 'fa',
           }),
         });
       },
@@ -235,31 +243,44 @@ fetch('data/uni_point.geojson')
         layer.bindPopup(popupContent);
       },
     }).addTo(map);
-    var universityListSidebar = document.getElementById("universityListSidebar");
+    var universityListSidebar = document.getElementById('universityListSidebar');
 
-    // Loop melalui fitur universitas dan tambahkan opsi ke dalam dropdown
     data.features.forEach(function (feature) {
-      var universityName = feature.properties.Nama; // Sesuaikan dengan atribut yang sesuai
-      var listItem = document.createElement("li");
-      var link = document.createElement("a");
-      link.href = "#";
+      var universityName = feature.properties.Nama; // Update with the appropriate attribute
+      var listItem = document.createElement('li');
+      var link = document.createElement('a');
+      link.href = '#';
       link.textContent = universityName;
 
-      link.addEventListener("click", function () {
-        // Fungsi untuk melakukan zoom pada peta ke lokasi universitas
+      link.addEventListener('click', function () {
+        // Zoom to the university location
         zoomToUniversity(feature.geometry.coordinates);
+
+        // Highlight the selected university in the list
+        var allLinks = universityListSidebar.querySelectorAll('a');
+        allLinks.forEach(function (item) {
+          item.classList.remove('selected');
+        });
+        link.classList.add('selected');
+
+        // Close the sidebar
+        sidebar.classList.remove('visible');
+        adjustToggleBtnPosition();
       });
 
       listItem.appendChild(link);
       universityListSidebar.appendChild(listItem);
     });
+
+    // Add a class to the dropdown list container
+    universityListSidebar.classList.add('dropdown-list');
   })
   .catch((error) => {
     console.error('Error loading uni_point.geojson:', error);
   });
 
 function zoomToUniversity(coordinates) {
-  map.setView([coordinates[1], coordinates[0]], 18); // 18 adalah level zoom yang sesuai, sesuaikan sesuai kebutuhan
+  map.setView([coordinates[1], coordinates[0]], 15); // 18 adalah level zoom yang sesuai, sesuaikan sesuai kebutuhan
 }
 // Fungsi untuk menyaring data berdasarkan nama cafe
 var searchInput = document.getElementById('searchCafeInput');
@@ -304,33 +325,38 @@ var toggleSidebarBtn = document.getElementById('toggleSidebarBtn');
 var sidebar = document.getElementById('sidebar');
 
 // Fungsi untuk menyesuaikan posisi tombol
-function adjustToggleBtnPosition() {
-  var sidebarWidth = sidebar.offsetWidth;
-  var toggleBtnWidth = toggleSidebarBtn.offsetWidth;
-  var expandedPosition = sidebarWidth + 10; // Sesuaikan nilai ini jika diperlukan
-  var collapsedPosition = 10; // Sesuaikan nilai ini jika diperlukan
+document.addEventListener('DOMContentLoaded', function () {
+  var toggleSidebarBtn = document.getElementById('toggleSidebarBtn');
+  var toggleIcon = document.getElementById('toggleIcon');
 
-  if (sidebar.classList.contains('visible')) {
-    toggleSidebarBtn.style.left = expandedPosition + 'px';
-  } else {
-    toggleSidebarBtn.style.left = collapsedPosition + 'px';
+  toggleSidebarBtn.addEventListener('click', function () {
+    // Toggle the icon class between 'fa-chevron-right' and 'fa-chevron-left'
+    toggleIcon.classList.toggle('fa-chevron-right');
+    toggleIcon.classList.toggle('fa-chevron-left');
+
+    // Toggle the 'visible' class on the sidebar
+    sidebar.classList.toggle('visible');
+
+    // Call the adjustToggleBtnPosition function to adjust the button position
+    adjustToggleBtnPosition();
+  });
+
+  function adjustToggleBtnPosition() {
+    var sidebarWidth = sidebar.offsetWidth;
+    var toggleBtnWidth = toggleSidebarBtn.offsetWidth;
+    var expandedPosition = sidebarWidth + 10; // Adjust as needed
+    var collapsedPosition = 10; // Adjust as needed
+
+    if (sidebar.classList.contains('visible')) {
+      toggleSidebarBtn.style.left = expandedPosition + 'px';
+    } else {
+      toggleSidebarBtn.style.left = collapsedPosition + 'px';
+    }
   }
-}
 
-// Panggil fungsi untuk pertama kali dan saat jendela diubah ukurannya
-adjustToggleBtnPosition();
-window.addEventListener('resize', adjustToggleBtnPosition);
-
-// Fungsi untuk menangani klik pada tombol toggle
-toggleSidebarBtn.addEventListener('click', function () {
-  if (sidebar.classList.contains('visible')) {
-    sidebar.classList.remove('visible');
-  } else {
-    sidebar.classList.add('visible');
-  }
-
-  // Panggil kembali fungsi untuk menyesuaikan posisi tombol
+  // Call adjustToggleBtnPosition initially and on window resize
   adjustToggleBtnPosition();
+  window.addEventListener('resize', adjustToggleBtnPosition);
 });
 
 function updateTable() {
@@ -345,20 +371,32 @@ function updateTable() {
   visibleFeatures.forEach(function (layer) {
     var cafeInfo = layer.feature.properties;
     var row = document.createElement('tr');
+    var starIcon = '<i class="fas fa-star yellow-star"></i>'; // Ikon bintang Font Awesome
+
+    var score = cafeInfo.totalScore / 10; // Menghitung skor yang dibagi dengan 10
+
     row.innerHTML = `
       <td>${cafeInfo.title}</td>
-    `;
+      <td>${score.toFixed(1)} ${starIcon}</td>
+      <td>${
+        cafeInfo.url
+          ? `<a class="text" href="${layer.feature.properties.url}" target="_blank" class="popup-button">
+      <img src="dist/images/gmaps.png" alt="Google Maps" class="button-icon">
+      Google Maps
+    </a>`
+          : '-'
+      }</td>`;
 
     // Menambahkan event listener untuk efek hover pada baris tabel
     row.addEventListener('mouseenter', function () {
       layer.setStyle({
-        fillOpacity: 0.3, // Warna berubah saat kursor berada di atas baris tabel
+        fillOpacity: 0.3,
       });
     });
 
     row.addEventListener('mouseleave', function () {
       layer.setStyle({
-        fillOpacity: 0, // Mengembalikan ke hollow saat kursor keluar dari baris tabel
+        fillOpacity: 0,
       });
     });
 
@@ -369,6 +407,55 @@ function updateTable() {
     cafeTableBody.appendChild(row);
   });
 }
+
+var cafes = [
+  { name: 'Renan', cafe: 'Lavana Coffee', ig: 'ramjirenanda.s', location: 'https://goo.gl/maps/ZDavCvUQ4LnBAQ748' },
+  { name: 'Michelle', cafe: 'Svarga Flora Coffee & Plants', ig: 'mchllhans', location: 'https://goo.gl/maps/M7Bed1BAoy38sU849' },
+  { name: 'Zani', cafe: 'SiNERGI Co Working Space & Network Space', ig: 'raidanazn', location: 'https://goo.gl/maps/jDonfpVbrrVuMhRq8' },
+];
+function loadCards() {
+  var cardModalBody = document.getElementById('cardModalBody');
+  cardModalBody.innerHTML = ''; // Clear previous content
+
+  cafes.forEach(function (cafe) {
+    var card = document.createElement('div');
+    card.className = 'card';
+    var cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    var cardTitle = document.createElement('h5');
+    cardTitle.className = 'card-title';
+
+    var instagramLink = document.createElement('a');
+    instagramLink.className = 'instagram-link';
+    instagramLink.href = 'https://www.instagram.com/' + cafe.ig;
+    instagramLink.textContent = cafe.name;
+
+    var mapsLink = document.createElement('a');
+    mapsLink.className = 'maps-link';
+    mapsLink.href = cafe.location;
+    mapsLink.textContent = cafe.cafe;
+
+    var mapsIcon = document.createElement('img');
+    mapsIcon.src = 'dist/images/gmaps.png'; // Change to the correct path
+    mapsIcon.width = 20; // Set the width in pixels
+    mapsIcon.height = 30; // Set the height in pixels
+
+    cardTitle.appendChild(instagramLink);
+    cardTitle.appendChild(document.createTextNode(' - '));
+    cardTitle.appendChild(mapsIcon); // Add the Google Maps icon
+    cardTitle.appendChild(document.createTextNode(' '));
+    cardTitle.appendChild(mapsLink);
+
+    cardBody.appendChild(cardTitle);
+    card.appendChild(cardBody);
+    cardModalBody.appendChild(card);
+  });
+}
+
+// When the "Rekomendasi Cafe dari Kami" modal is shown, call the loadCards() function
+$('#cardModal').on('show.bs.modal', function (event) {
+  loadCards();
+});
 
 // Fungsi untuk menampilkan fitur pada peta berdasarkan nama cafe
 function zoomToFeatureOnMap(cafeName) {
@@ -393,5 +480,3 @@ function zoomToFeatureOnMap(cafeName) {
 map.on('moveend', function () {
   updateTable();
 });
-
-
